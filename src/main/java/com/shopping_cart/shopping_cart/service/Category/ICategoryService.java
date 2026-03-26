e1ddace1888ff5547c9d36c7a6a08f61778180aa
HeliShoter1 <96410469+HeliShoter1@users.noreply.github.com>
2026-03-26 15:51:05 +0700

    fix again redis category and get all categories

--- src/main/java/com/shopping_cart/shopping_cart/service/Category/ICategoryService.java
+++ src/main/java/com/shopping_cart/shopping_cart/service/Category/ICategoryService.java
@@ -8,7 +8,7 @@ import com.shopping_cart.shopping_cart.model.Category;
 public interface ICategoryService {
     Category getCategoryById(Long id);
     Category getCategoryByName(String name);
-    List<Category> getAllCategories();
+    List<Category> getAllCategories(Long cursor, Long limit);
     Category addCategory(CategoryDto category);
     Category updateCategory(Category category, Long id);
     void deleteCategoryById(Long id);
